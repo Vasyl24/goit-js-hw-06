100e7ecc62d299fe49860d89d53d834049bea9d7
Vasyl Shaposhnyk <aries240302@gmail.com>
2023-03-25 23:04:38 +0200

    Change task 7, 8 and remove src folder

--- js/task-08.js
+++ js/task-08.js
@@ -6,20 +6,12 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   if (userEmail.value === '' || userPassword.value === '') {
     alert('Всі поля повинні бути заповнені');
-    // return;
   } else {
     const userForm = {
       email: userEmail.value,
       password: userPassword.value,
     };
     console.log(userForm);
-
-    // const {
-    //   elements: { email, password },
-    // } = event.currentTarget;
-    // console.log(email.value, password.value);
-    //Якщо робити з elements то буде виводитись не об'єкт,
-    // а просто значення
-    form.reset();
   }
+  form.reset();
 });
